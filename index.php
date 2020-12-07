9ed7d5ee4ee207da0f96a3e3f22f83e14eeb28d9
Paweł <kalicki.paul@icloud.com>
2020-12-07 12:59:20 +0100

    :zap: Repaired map

--- index.php
+++ index.php
@@ -176,19 +176,17 @@ else
         </div>
       </div>
 
-    <!-- Map -->
-      <div class="container-fluid">
+      <!-- Map -->
+      <div class="container my-4">
         <div class="jumbotron" >
-          <div class="row text-center">
             <section id="contact" class="map">
-              <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+              <iframe class="text-center" width="100%" height="80%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
                 src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
               <br />
               <small>
                 <a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A"></a>
               </small>
             </section>
-          </div>
         </div>
       </div>
 
@@ -197,7 +195,7 @@ else
 
   </div>
   <!-- /.container -->
-
+  
   <!-- Footer -->
   <footer class="py-5 bg-dark">
     <div class="container">
